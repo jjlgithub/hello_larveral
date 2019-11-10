ee86709136f124fa1072079a04e18bb3c1ac88dc
chenjiale <1987909748@qq.com>
2019-11-10 20:19:46 +0800

    '注册'

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');//首页
-Route::get('/help','StaticPagesController@help');//帮助
-Route::get('/about','StaticPagesController@about');//关于我们
+Route::get('/', 'StaticPagesController@home')->name('home');//首页
+Route::get('/help','StaticPagesController@help')->name('help');//帮助
+Route::get('/about','StaticPagesController@about')->name('about');//关于我们
+
+Route::get('signup','UsersController@create')->name('signup');//注册
